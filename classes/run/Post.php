194077d96117f8792a6630e7bb30ef423e065337
amrfahmy5 <elkingamr46@yahoo.com>
2019-04-12 00:23:34 +0200

    final

--- classes/run/Post.php
+++ classes/run/Post.php
@@ -12,7 +12,7 @@
     public $TotalScore;
     public $Duration;
 
-    public $QID ;
+    public $QId ;
     public $QuestId;
     public $Quetion ;
     public$Valid ;
@@ -41,16 +41,16 @@
 
    public function read_single() {
           // Create query
-     //     $query = 'SELECT * from question where QID=?';
-      
-      $query = 'SELECT * FROM  Quiz  c JOIN question p ON p.QID = c.QuizId WHERE  c.QuizId = ?';
+      //    $query = 'SELECT * from question where QID=?';
+       // echo $QID;
+      $query = 'SELECT * FROM  question p  LEFT JOIN Quiz  c ON p.QID = c.QuizId WHERE  P.QID = ?';
 
                                     
           // Prepare statement
           $stmt = $this->conn->prepare($query);
 
           // Bind ID
-          $stmt->bindParam(1, $this->QuizId);
+          $stmt->bindParam(1, $this->QID);
 
           // Execute query
           $stmt->execute();
--- classes/run/read_single.php
+++ classes/run/read_single.php
@@ -14,48 +14,82 @@
   $post = new Post($db);
 
   // Get ID
-  $post->QuizId = isset($_GET['QuizId']) ? $_GET['QuizId'] : die();
+  $post->QID = isset($_GET['QuizId']) ? $_GET['QuizId'] : die();
 
   // Get post
   $result = $post->read_single();
+// Get row count
+  $num = $result->rowCount();
 
-$num = $result->rowCount();
+  // Check if any posts
   if($num > 0) {
-  // Post array
+    // Post array
     $posts_arr = array();
     // $posts_arr['data'] = array();
-    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+
+    //get info for Quiz
+    $row = $result->fetch(PDO::FETCH_ASSOC) ;
       extract($row);
+
       $post_item = array(
-       'QuizId' => $QuizId,
+       
+        'QuizId' => $QuizId,
         'QuizTitle' => $QuizTitle,
+        
         'QuizDescription' => $QuizDescription,
         'TotalScore' => $TotalScore,
         'Duration' => $Duration,
-        
-       // 'QID' => $QId,
+ 
+      );
+      array_push($posts_arr, $post_item);
+      unset($post_item); // $foo is gone
+      
+$post_item = array(); // $foo is here again
+//get info for each question
+while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+  extract($row);
+        $post_item2 = array(
+        'QId' => $QId ,
         'QuestId' => $QuestId,        
         'Quetion' => $Quetion,
         'Valid' => $Valid,
         'FakeAns1' => $FakeAns1,
         'FakeAns2' => $FakeAns2,
-        'FakeAns3' => $FakeAns3
+        'FakeAns3' => $FakeAns3,
+     
       );
+        
+  array_push($post_item, $post_item2);
+}
       // Push to "data"
       array_push($posts_arr, $post_item);
+
+
       // array_push($posts_arr['data'], $post_item);
-    }
+ //   }
+
     // Turn to JSON & output
     echo json_encode($posts_arr);
-}
-  else
-  {
+
+  } else {
     // No Posts
     echo json_encode(
       array('message' => 'No Posts Found')
     );
   }
-  // Make JSON
-  print_r(json_encode($posts_arr));
-
-  ?>
+    /*   'QuizId' => $QuizId,
+        'QuizTitle' => $QuizTitle,
+        'QuizDescription' => $QuizDescription,
+        'TotalScore' => $TotalScore,
+        'Duration' => $Duration,
+      
+        'QID' => $QId,
+        'QuestId' => $QuestId,        
+        'Quetion' => $Quetion,
+        'Valid' => $Valid,
+        'FakeAns1' => $FakeAns1,
+        'FakeAns2' => $FakeAns2,
+        'FakeAns3' => $FakeAns3,
+        
+*/
+        ?>
