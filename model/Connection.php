4b5027562291acb86d0d2c84d7655e1ba458a8fc
amrfahmy5 <elkingamr46@yahoo.com>
2019-04-29 03:45:11 +0200

    nothing

--- model/Connection.php
+++ model/Connection.php
@@ -2,7 +2,7 @@
 
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'test');
 define('DB_DATABASE', 'quizdb');
 
 function connect(&$con)
